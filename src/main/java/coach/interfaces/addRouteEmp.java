e34fafad88ffb77eeb5cc058b8947a0ca599545d
ckchamara <ckchamara22@gmail.com>
2021-11-02 04:58:23 +0530

    ui enhanced

--- src/main/java/coach/interfaces/addRouteEmp.java
+++ src/main/java/coach/interfaces/addRouteEmp.java
@@ -6,44 +6,168 @@ package coach.interfaces;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
 
 public class addRouteEmp extends JPanel implements ActionListener {
-    String[] jcomp1Items = {"Item 1", "Item 2", "Item 3"};
-    String[] jcomp3Items = {"Item 1", "Item 2", "Item 3"};
-    String[] jcomp24Items = {"Item 1", "Item 2", "Item 3"};
-    String[] jcomp25Items = {"Item 1", "Item 2", "Item 3"};
-    String[] jcomp26Items = {"Item 1", "Item 2", "Item 3"};
+    String[] EUCountries = {"Austria","Italy", "Belgium","Latvia", "Bulgaria",	"Lithuania", "Croatia",	"Luxembourg", "Cyprus",	"MaltaCzechia",	"Netherlands", "Denmark",	"Poland", "Estonia",	"Portugal", "Finland",	"Romania", "France",	"Slovakia", "Germany",	"Slovenia", "Greece",	"Spain", "Hungary",	"Sweden", "Ireland"	};
+    String[] Monthjcomp24Items = {    "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",};
+    String[] Datejcomp25Items = {"1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30"
+    };
+
+    String[] Miuutes = {"1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"
+    };
+    String[] Hourjcomp26Items = {"1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"
+    };
+
+    String[] Yearcomp26Items = {"2021",
+            "2022",
+            "2023",
+            "2024",
+            "2025",
+            "2026",
+            "2027",
+            "2028",
+            "2029",
+            "2030"
+    };
+
+    String[] seatsNos = {"42","48","54"};
 
     JFrame frame = new JFrame ("Add Route Employee");
 
-    JComboBox jcomp1 = new JComboBox (jcomp1Items);
+    JComboBox jcomp1 = new JComboBox (EUCountries);
     JLabel jcomp2 = new JLabel ("Origin");
-    JComboBox jcomp3 = new JComboBox (jcomp3Items);
+    JComboBox jcomp3 = new JComboBox (EUCountries);
     JLabel jcomp4 = new JLabel ("Departure");
-    JTextField jcomp5 = new JTextField (5);
+    JTextField jcomp5 = new JTextField ("CityO");
     JLabel jcomp6 = new JLabel ("City");
-    JTextField jcomp7 = new JTextField (5);
+    JTextField jcomp7 = new JTextField ("CityD");
     JLabel jcomp8 = new JLabel ("City");
-    JTextField jcomp9 = new JTextField (5);
-    JLabel jcomp10 = new JLabel ("Route ID");
-    JTextField jcomp11 = new JTextField (5);
-    JLabel jcomp12 = new JLabel ("Price");
-    JLabel jcomp13 = new JLabel ("Seats");
+    JTextField jcomp9 = new JTextField ("Price");
+    JLabel jcomp10 = new JLabel ("Price");
+    JLabel jcomp12 = new JLabel ("Seats");
+    JComboBox Seatsjcomp11 = new JComboBox (seatsNos);
     JCheckBox jcomp14 = new JCheckBox ("Sun");
     JCheckBox jcomp15 = new JCheckBox ("Mon");
-    JTextField jcomp16 = new JTextField (5);
     JCheckBox jcomp17 = new JCheckBox ("Fri");
     JCheckBox jcomp18 = new JCheckBox ("Sat");
     JCheckBox jcomp19 = new JCheckBox ("Thr");
     JCheckBox jcomp20 = new JCheckBox ("Tue");
     JCheckBox jcomp21 = new JCheckBox ("Wed");
-    JButton jcomp22 = new JButton ("Add Route");
-    JTextField jcomp23 = new JTextField (5);
-    JComboBox jcomp24 = new JComboBox (jcomp24Items);
-    JComboBox jcomp25 = new JComboBox (jcomp25Items);
-    JComboBox jcomp26 = new JComboBox (jcomp26Items);
-    JTextField jcomp27 = new JTextField (5);
+    JButton addRouteBtn = new JButton ("Add Route");
+    JComboBox Yearjcomp23 = new JComboBox (Yearcomp26Items);
+    JComboBox jcomp24 = new JComboBox (Monthjcomp24Items);
+    JComboBox jcomp25 = new JComboBox (Datejcomp25Items);
+    JComboBox jcomp26 = new JComboBox (Hourjcomp26Items);
+    JComboBox Minjcomp27 = new JComboBox (Miuutes);
     JLabel jcomp28 = new JLabel ("Year");
     JLabel jcomp29 = new JLabel ("Date");
     JLabel jcomp30 = new JLabel ("Month");
@@ -78,23 +202,21 @@ public class addRouteEmp extends JPanel implements ActionListener {
         jcomp8.setBounds (560, 10, 100, 25);
         jcomp9.setBounds (35, 125, 150, 25);
         jcomp10.setBounds (35, 100, 100, 25);
-        jcomp11.setBounds (35, 180, 100, 25);
+        Seatsjcomp11.setBounds (35, 180, 100, 25);
         jcomp12.setBounds (35, 155, 100, 25);
-        jcomp13.setBounds (35, 210, 100, 25);
         jcomp14.setBounds (230, 120, 50, 25);
         jcomp15.setBounds (230, 145, 50, 25);
-        jcomp16.setBounds (35, 235, 100, 25);
         jcomp17.setBounds (305, 145, 55, 25);
         jcomp18.setBounds (305, 170, 55, 25);
         jcomp19.setBounds (305, 120, 55, 25);
         jcomp20.setBounds (230, 170, 55, 25);
         jcomp21.setBounds (230, 195, 65, 25);
-        jcomp22.setBounds (320, 305, 100, 25);
-        jcomp23.setBounds (435, 130, 80, 25);
+        addRouteBtn.setBounds (320, 305, 100, 25);
+        Yearjcomp23.setBounds (435, 130, 80, 25);
         jcomp24.setBounds (520, 130, 100, 25);
         jcomp25.setBounds (625, 130, 100, 25);
         jcomp26.setBounds (475, 195, 100, 25);
-        jcomp27.setBounds (580, 195, 80, 25);
+        Minjcomp27.setBounds (580, 195, 80, 25);
         jcomp28.setBounds (435, 105, 40, 25);
         jcomp29.setBounds (625, 105, 40, 25);
         jcomp30.setBounds (520, 105, 45, 25);
@@ -113,23 +235,21 @@ public class addRouteEmp extends JPanel implements ActionListener {
         frame.add (jcomp8);
         frame.add (jcomp9);
         frame.add (jcomp10);
-        frame.add (jcomp11);
+        frame.add (Seatsjcomp11);
         frame.add (jcomp12);
-        frame.add (jcomp13);
         frame.add (jcomp14);
         frame.add (jcomp15);
-        frame.add (jcomp16);
         frame.add (jcomp17);
         frame.add (jcomp18);
         frame.add (jcomp19);
         frame.add (jcomp20);
         frame.add (jcomp21);
-        frame.add (jcomp22);
-        frame.add (jcomp23);
+        frame.add (addRouteBtn);
+        frame.add (Yearjcomp23);
         frame.add (jcomp24);
         frame.add (jcomp25);
         frame.add (jcomp26);
-        frame.add (jcomp27);
+        frame.add (Minjcomp27);
         frame. add (jcomp28);
         frame.add (jcomp29);
         frame.add (jcomp30);
@@ -139,12 +259,15 @@ public class addRouteEmp extends JPanel implements ActionListener {
     }
 
     public void actionEvent() {
-        jcomp22.addActionListener(this);
+        addRouteBtn.addActionListener(this);
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == addRouteBtn) {
+
+        }
 
     }
 
