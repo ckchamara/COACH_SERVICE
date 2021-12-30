77d6d5c26fafdbbdeb4784806fb4134b2dab0641
ckchamara <ckchamara22@gmail.com>
2021-12-30 18:42:01 +0530

    EMPLOYEE  reg ok

--- src/main/java/coach/interfaces/EmployeeRegistrationForm.java
+++ src/main/java/coach/interfaces/EmployeeRegistrationForm.java
@@ -14,23 +14,14 @@ public class EmployeeRegistrationForm extends JFrame implements ActionListener {
     JFrame frame = new JFrame("Customer Registration Form");
     String[] gender={"Male","Female"};
     JLabel firstNameLabel =new JLabel("FIRST NAME");
-    JLabel genderLabel=new JLabel("GENDER");
-    JLabel lastNameLabel =new JLabel("LAST NAME");
-    JLabel passwordLabel=new JLabel("PASSWORD");
-    JLabel DOBLabel =new JLabel("DOB");
-    JLabel AddressLable =new JLabel("ADDRESS");
-    JLabel emailLabel=new JLabel("EMAIL");
-    JLabel MobileNoLabel=new JLabel("MOBILE NO");
+    JLabel passwordlabel =new JLabel("LAST NAME");
+    JLabel lastNameLabel =new JLabel("PASSWORD");
 
 
-    JTextField nameTextField=new JTextField();
-    JComboBox genderComboBox=new JComboBox(gender);
+    JTextField firstnameTextField =new JTextField();
+    JPasswordField passwordField =new JPasswordField();
     JTextField lastNameTextField =new JTextField();
-    JPasswordField passwordField=new JPasswordField();
-    JTextField DOBField =new JTextField();
-    JTextField AddressTextField =new JTextField();
-    JTextField emailTextField=new JTextField();
-    JTextField MobileTextField=new JTextField();
+//    JPasswordField passwordField=new JPasswordField();
 
     JButton registerButton=new JButton("REGISTER");
     JButton resetButton=new JButton("RESET");
@@ -46,7 +37,7 @@ public class EmployeeRegistrationForm extends JFrame implements ActionListener {
     public void createWindow()
     {
 
-        frame.setBounds(40,40,380,600);
+        frame.setBounds(40,40,380,380);
         frame.getContentPane().setBackground(Color.lightGray);
         frame.getContentPane().setLayout(null);
         frame.setVisible(true);
@@ -56,44 +47,24 @@ public class EmployeeRegistrationForm extends JFrame implements ActionListener {
     public void setLocationAndSize()
     {
         firstNameLabel.setBounds(20,20,100,70);
-        genderLabel.setBounds(20,70,80,70);
+        passwordlabel.setBounds(20,70,80,70);
         lastNameLabel.setBounds(20,120,100,70);
-        passwordLabel.setBounds(20,170,100,70);
-        DOBLabel.setBounds(20,220,140,70);
-        AddressLable.setBounds(20,270,100,70);
-        emailLabel.setBounds(20,320,100,70);
-        MobileNoLabel.setBounds(20,370,100,70);
 
-        nameTextField.setBounds(180,43,165,23);
-        genderComboBox.setBounds(180,93,165,23);
-        lastNameTextField.setBounds(180,143,165,23);
-        passwordField.setBounds(180,193,165,23);
-        DOBField.setBounds(180,243,165,23);
-        AddressTextField.setBounds(180,293,165,23);
-        emailTextField.setBounds(180,343,165,23);
-        MobileTextField.setBounds(180,393,165,23);
+        firstnameTextField.setBounds(180,43,165,23);
+        passwordField.setBounds(180,143,165,23);
+        lastNameTextField.setBounds(180,93,165,23);
 
-        registerButton.setBounds(70,450,100,35);
-        resetButton.setBounds(220,450,100,35);
+        registerButton.setBounds(70,190,100,35);
+        resetButton.setBounds(220,190,100,35);
     }
     public void addComponentsToFrame()
     {
         frame.add(firstNameLabel);
-        frame.add(genderLabel);
+        frame.add(passwordlabel);
         frame.add(lastNameLabel);
-        frame.add(passwordLabel);
-        frame.add(DOBLabel);
-        frame.add(AddressLable);
-        frame.add(emailLabel);
-        frame.add(nameTextField);
-        frame.add(genderComboBox);
-        frame.add(lastNameTextField);
+        frame.add(firstnameTextField);
         frame.add(passwordField);
-        frame.add(DOBField);
-        frame.add(AddressTextField);
-        frame.add(emailTextField);
-        frame.add(MobileNoLabel);
-        frame.add(MobileTextField);
+        frame.add(lastNameTextField);
 
         frame.add(registerButton);
         frame.add(resetButton);
@@ -113,30 +84,20 @@ public class EmployeeRegistrationForm extends JFrame implements ActionListener {
 //            frame.setVisible(false);
 
             String firstName = lastNameTextField.getText();
-            String gender = genderComboBox.getSelectedItem().toString();
+            String password = passwordField.getText();
             String lastName = lastNameTextField.getText();
-            String Password = passwordField.getText();
-            String DOB = DOBField.getText();
-            String Address = AddressTextField.getText();
-            String Email = emailTextField.getText();
-            String MobileNo = MobileTextField.getText();
 
             try {
                 Connection conn = dbConnection.getInstance().getConnection();
 
-                String query = " insert into busbook.customers (`FirstName`, `Lastname`, `Email`, `LoginPassword`, `DOB`, `Gender`, `PhoneNumber`, `Address`)" +
-                        " values (?, ?, ?, ?, ? ,?, ?, ?)";
+                String query = " insert into busbook.employees (`firstName`, `lastName`, `password`)" +
+                        " values (?, ?, ?)";
 
                 // create the mysql insert preparedstatement
                 PreparedStatement preparedStmt = conn.prepareStatement(query);
                 preparedStmt.setString(1, firstName);
                 preparedStmt.setString(2, lastName);
-                preparedStmt.setString(3, Email);
-                preparedStmt.setString(4, Password);
-                preparedStmt.setString(5,DOB);
-                preparedStmt.setString(6,gender);
-                preparedStmt.setString(7,MobileNo);
-                preparedStmt.setString(8, Address);
+                preparedStmt.setString(3, password);
 
                 // execute the preparedstatement
                 preparedStmt.execute();
